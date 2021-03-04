d1243e01a0a34bc9da151b056a65d765405da461
Levi Durfee <levi.durfee@gmail.com>
2021-03-04 11:42:29 -0500

    Move Caddy specific/Caddy config code to a separate Go file

---
+++ caddyfile.go
@@ -0,0 +1,165 @@
+package httpcache
+
+import (
+	"strconv"
+	"sync"
+
+	"github.com/caddyserver/caddy/v2"
+	"github.com/caddyserver/caddy/v2/caddyconfig/caddyfile"
+	"github.com/caddyserver/caddy/v2/caddyconfig/httpcaddyfile"
+	"github.com/caddyserver/caddy/v2/modules/caddyhttp"
+	"github.com/golevi/cache-handler/config"
+	"github.com/golevi/cache-handler/handlers"
+	"github.com/golevi/cache-handler/stores"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+)
+
+var (
+	cfg *config.Config
+)
+
+func init() {
+	caddy.RegisterModule(Cache{})
+	httpcaddyfile.RegisterGlobalOption("cache", parseCaddyfileGlobalOption)
+	httpcaddyfile.RegisterHandlerDirective("cache", parseCaddyfileHandlerDirective)
+}
+
+var httpMetrics = struct {
+	init        sync.Once
+	cacheHit    *prometheus.CounterVec
+	cacheMiss   *prometheus.CounterVec
+	cacheBypass *prometheus.CounterVec
+}{
+	init: sync.Once{},
+}
+
+// CaddyModule returns the Caddy module information.
+func (Cache) CaddyModule() caddy.ModuleInfo {
+	return caddy.ModuleInfo{
+		ID:  "http.handlers.cache",
+		New: func() caddy.Module { return new(Cache) },
+	}
+}
+
+func parseCaddyfileGlobalOption(d *caddyfile.Dispenser) (interface{}, error) {
+	cfg = &config.Config{}
+	for d.Next() {
+		for d.NextBlock(0) {
+			switch d.Val() {
+			case "host":
+				if !d.NextArg() {
+					return nil, d.ArgErr()
+				}
+
+				cfg.Host = d.Val()
+			case "type":
+				if !d.NextArg() {
+					return nil, d.ArgErr()
+				}
+
+				cfg.Type = d.Val()
+			}
+		}
+	}
+
+	return nil, nil
+}
+
+func parseCaddyfileHandlerDirective(h httpcaddyfile.Helper) (caddyhttp.MiddlewareHandler, error) {
+	c := &Cache{}
+	if cfg != nil {
+		c.Config = *cfg
+	}
+
+	err := c.UnmarshalCaddyfile(h.Dispenser)
+	if err != nil {
+		return nil, err
+	}
+
+	return c, nil
+}
+
+// UnmarshalCaddyfile parses plugin settings from Caddyfile.
+//
+//	{
+//		order cache first
+//		cache {
+// 			type <redis>|<file>
+//			host localhost:6379
+// 		}
+//	}
+//
+//	cache {
+// 		expire 120                              # Cache expiration in seconds
+// 		method post                             # HTTP Methods you want to bypass
+// 		bypass wp-admin wp-login.php system     # WordPress and ExpressionEngine
+// 		# cookie exp_sessionid                  # ExpressionEngine
+// 		cookie wordpress_logged_in_.*           # WordPress
+//	}
+//
+// This may change.
+func (c *Cache) UnmarshalCaddyfile(d *caddyfile.Dispenser) error {
+	for d.Next() {
+		for d.NextBlock(0) {
+			switch d.Val() {
+			case "bypass":
+				c.Config.Bypass = d.RemainingArgs()
+			case "cookie":
+				c.Config.Cookie = d.RemainingArgs()
+			case "expire":
+				expire, _ := strconv.Atoi(d.RemainingArgs()[0])
+				c.Config.Expire = expire
+			case "method":
+				c.Config.Method = d.RemainingArgs()
+			}
+		}
+	}
+
+	return nil
+}
+
+// Provision _
+func (c *Cache) Provision(ctx caddy.Context) error {
+	const ns, sub = "caddy", "http"
+
+	basicLabels := []string{"handler"}
+	httpMetrics.cacheHit = promauto.NewCounterVec(prometheus.CounterOpts{
+		Namespace: ns,
+		Subsystem: sub,
+		Name:      "requests_cache_hit",
+		Help:      "Counter of HTTP cache hit requests",
+	}, basicLabels)
+	httpMetrics.cacheMiss = promauto.NewCounterVec(prometheus.CounterOpts{
+		Namespace: ns,
+		Subsystem: sub,
+		Name:      "requests_cache_miss",
+		Help:      "Counter of HTTP cache miss requests",
+	}, basicLabels)
+	httpMetrics.cacheBypass = promauto.NewCounterVec(prometheus.CounterOpts{
+		Namespace: ns,
+		Subsystem: sub,
+		Name:      "requests_cache_bypass",
+		Help:      "Counter of HTTP cache bypass requests",
+	}, basicLabels)
+
+	c.logger = ctx.Logger(c)
+	switch c.Config.Type {
+	case "redis":
+		c.Store = stores.NewRedisStore(c.Config.Host)
+	case "file":
+		c.Store = stores.NewFileStore()
+	}
+
+	c.Deciders = append(c.Deciders, handlers.URI)
+	c.Deciders = append(c.Deciders, handlers.Method)
+	c.Deciders = append(c.Deciders, handlers.Cookie)
+
+	return nil
+}
+
+var (
+	_ caddy.Provisioner           = (*Cache)(nil)
+	_ caddyhttp.MiddlewareHandler = (*Cache)(nil)
+	_ caddyfile.Unmarshaler       = (*Cache)(nil)
+)
--- httpcache.go
+++ httpcache.go
@@ -5,31 +5,14 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
-	"strconv"
-	"sync"
 	"time"
 
-	"github.com/caddyserver/caddy/v2"
-	"github.com/caddyserver/caddy/v2/caddyconfig/caddyfile"
-	"github.com/caddyserver/caddy/v2/caddyconfig/httpcaddyfile"
 	"github.com/caddyserver/caddy/v2/modules/caddyhttp"
 	"github.com/golevi/cache-handler/config"
-	"github.com/golevi/cache-handler/handlers"
 	"github.com/golevi/cache-handler/stores"
 	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/promauto"
 	"go.uber.org/zap"
 )
-
-var (
-	cfg *config.Config
-)
-
-func init() {
-	caddy.RegisterModule(Cache{})
-	httpcaddyfile.RegisterGlobalOption("cache", parseCaddyfileGlobalOption)
-	httpcaddyfile.RegisterHandlerDirective("cache", parseCaddyfileHandlerDirective)
-}
 
 // Cache stuff
 type Cache struct {
@@ -46,23 +29,6 @@ type cacheResponse struct {
 	Headers       http.Header `json:"headers"`
 	Body          []byte      `json:"body"`
 	ContentLength int64       `json:"content_length"`
-}
-
-var httpMetrics = struct {
-	init        sync.Once
-	cacheHit    *prometheus.CounterVec
-	cacheMiss   *prometheus.CounterVec
-	cacheBypass *prometheus.CounterVec
-}{
-	init: sync.Once{},
-}
-
-// CaddyModule returns the Caddy module information.
-func (Cache) CaddyModule() caddy.ModuleInfo {
-	return caddy.ModuleInfo{
-		ID:  "http.handlers.cache",
-		New: func() caddy.Module { return new(Cache) },
-	}
 }
 
 func (c *Cache) ServeHTTP(w http.ResponseWriter, r *http.Request, next caddyhttp.Handler) error {
@@ -165,128 +131,7 @@ func (c *Cache) ServeHTTP(w http.ResponseWriter, r *http.Request, next caddyhttp
 
 	return nil
 }
-func parseCaddyfileGlobalOption(d *caddyfile.Dispenser) (interface{}, error) {
-	cfg = &config.Config{}
-	for d.Next() {
-		for d.NextBlock(0) {
-			switch d.Val() {
-			case "host":
-				if !d.NextArg() {
-					return nil, d.ArgErr()
-				}
-
-				cfg.Host = d.Val()
-			case "type":
-				if !d.NextArg() {
-					return nil, d.ArgErr()
-				}
-
-				cfg.Type = d.Val()
-			}
-		}
-	}
-
-	return nil, nil
-}
-
-func parseCaddyfileHandlerDirective(h httpcaddyfile.Helper) (caddyhttp.MiddlewareHandler, error) {
-	c := &Cache{}
-	if cfg != nil {
-		c.Config = *cfg
-	}
-
-	err := c.UnmarshalCaddyfile(h.Dispenser)
-	if err != nil {
-		return nil, err
-	}
-
-	return c, nil
-}
-
-// UnmarshalCaddyfile parses plugin settings from Caddyfile.
-//
-//	{
-//		order cache first
-//		cache {
-// 			type <redis>|<file>
-//			host localhost:6379
-// 		}
-//	}
-//
-//	cache {
-// 		expire 120                              # Cache expiration in seconds
-// 		method post                             # HTTP Methods you want to bypass
-// 		bypass wp-admin wp-login.php system     # WordPress and ExpressionEngine
-// 		# cookie exp_sessionid                  # ExpressionEngine
-// 		cookie wordpress_logged_in_.*           # WordPress
-//	}
-//
-// This may change.
-func (c *Cache) UnmarshalCaddyfile(d *caddyfile.Dispenser) error {
-	for d.Next() {
-		for d.NextBlock(0) {
-			switch d.Val() {
-			case "bypass":
-				c.Config.Bypass = d.RemainingArgs()
-			case "cookie":
-				c.Config.Cookie = d.RemainingArgs()
-			case "expire":
-				expire, _ := strconv.Atoi(d.RemainingArgs()[0])
-				c.Config.Expire = expire
-			case "method":
-				c.Config.Method = d.RemainingArgs()
-			}
-		}
-	}
-
-	return nil
-}
-
-// Provision _
-func (c *Cache) Provision(ctx caddy.Context) error {
-	const ns, sub = "caddy", "http"
-
-	basicLabels := []string{"handler"}
-	httpMetrics.cacheHit = promauto.NewCounterVec(prometheus.CounterOpts{
-		Namespace: ns,
-		Subsystem: sub,
-		Name:      "requests_cache_hit",
-		Help:      "Counter of HTTP cache hit requests",
-	}, basicLabels)
-	httpMetrics.cacheMiss = promauto.NewCounterVec(prometheus.CounterOpts{
-		Namespace: ns,
-		Subsystem: sub,
-		Name:      "requests_cache_miss",
-		Help:      "Counter of HTTP cache miss requests",
-	}, basicLabels)
-	httpMetrics.cacheBypass = promauto.NewCounterVec(prometheus.CounterOpts{
-		Namespace: ns,
-		Subsystem: sub,
-		Name:      "requests_cache_bypass",
-		Help:      "Counter of HTTP cache bypass requests",
-	}, basicLabels)
-
-	c.logger = ctx.Logger(c)
-	switch c.Config.Type {
-	case "redis":
-		c.Store = stores.NewRedisStore(c.Config.Host)
-	case "file":
-		c.Store = stores.NewFileStore()
-	}
-
-	c.Deciders = append(c.Deciders, handlers.URI)
-	c.Deciders = append(c.Deciders, handlers.Method)
-	c.Deciders = append(c.Deciders, handlers.Cookie)
-
-	return nil
-}
 
 func key(r *http.Request) string {
 	return "request:" + r.Method + ":" + r.Host + ":" + r.URL.Path
 }
-
-var (
-	_ caddy.Provisioner           = (*Cache)(nil)
-	_ caddyhttp.MiddlewareHandler = (*Cache)(nil)
-	_ caddyfile.Unmarshaler       = (*Cache)(nil)
-)
