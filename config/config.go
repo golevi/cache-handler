3da6b76855c0ebc172ee536458a7471c820f50f5
Levi Durfee <levi.durfee@gmail.com>
2021-03-10 12:24:57 -0500

    Config: add a cookie config struct [#2]

--- config/config.go
+++ config/config.go
@@ -1,12 +1,20 @@
 package config
 
+import "regexp"
+
+type cookieConfig struct {
+	Name string
+	Rege regexp.Regexp
+}
+
 // Config options
 type Config struct {
 	Type string `json:"type,omitempty"`
 	Host string `json:"host,omitempty"`
 
-	Bypass []string `json:"bypass"`
-	Method []string `json:"method"`
-	Expire int      `json:"expire"`
-	Cookie []string `json:"cookie"`
+	Bypass  []string       `json:"bypass"`
+	Method  []string       `json:"method"`
+	Expire  int            `json:"expire"`
+	Cookie  []string       `json:"cookie"`
+	Cookies []cookieConfig `json:"cookies"`
 }
